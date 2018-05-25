1af93b7ea787f604ebeeeb9373d0b3fe2b07a1bd
康佳兴 <kang.jiaxing@xcar.com.cn>
2018-05-25 17:55:49 +0800

    提交

---
+++ MoyaUseShow/MoyaUseShow/Token.swift
@@ -0,0 +1,76 @@
+//
+//  Token.swift
+//  moyaTest
+//
+//  Created by 康佳兴 on 2018/5/23.
+//  Copyright © 2018年 Kang. All rights reserved.
+//
+
+import Foundation
+
+class Token {
+    private var signKey: String = ""
+    
+    private var parameter: [String: Any]
+    
+    init(signKey: String, parameter: [String: Any]?) {
+        self.parameter = parameter ?? [:]
+        self.signKey = signKey
+    }
+    
+    ///转化为根据key值排序由value生成的字符串
+    var token: String {
+        parameter["signedKey"] = signKey
+        parameter["token"] = nil
+        return parameter.toKeyOrderedValueString().md5
+    }
+}
+
+
+
+extension Dictionary where Key == String, Value == Any {
+    func toKeyOrderedValueString(separator: String = "") -> String {
+        var stringDictionary: [String: Any] = [:]
+        for (key, value) in self {
+            stringDictionary[key] = value
+        }
+        let sortedKeys = stringDictionary.keys.sorted()
+        let values = sortedKeys.map { key -> String in
+            if let value = stringDictionary[key] {
+                return "\(value)"
+            }
+            return ""
+        }
+        return values.joined(separator: separator)
+    }
+}
+
+extension String {
+    var md5: String {
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        if let data = data(using: .utf8) {
+            data.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) -> Void in
+                CC_MD5(bytes, CC_LONG(data.count), &digest)
+            }
+        }
+        
+        var digestHex = ""
+        for index in 0..<Int(CC_MD5_DIGEST_LENGTH) {
+            digestHex += String(format: "%02x", digest[index])
+        }
+        
+        return digestHex
+    }
+}
+
+// MARK: - Helpers
+extension String {
+    var urlEscaped: String {
+        return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    }
+    
+    var utf8Encoded: Data {
+        return data(using: .utf8)!
+    }
+}
+
---
+++ MoyaUseShow/MoyaUseShow/基本用法.swift
@@ -0,0 +1,80 @@
+//
+//  基本用法.swift
+//  MoyaUseShow
+//
+//  Created by 康佳兴 on 2018/5/25.
+//  Copyright © 2018年 Kang. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+/*
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    let provider = MoyaProvider<MyService>()
+    
+    provider.request(.carInfo(carId: 1, cityId: 1)) { (result) in
+        switch result {
+        case let .success(response):
+            do {
+                let showJson = try response.mapJSON()
+                print(showJson, "7777")
+            } catch {
+                print(error)
+            }
+            
+        case let .failure(error):
+            print(error,"8888888")
+        }
+    }
+}
+*/
+
+private enum MyService {
+    case carInfo(carId:Int, cityId:Int)
+}
+
+// MARK: - TargetType Protocol Implementation
+extension MyService: TargetType {
+    var baseURL: URL {
+        switch self {
+        case .carInfo:
+            return URL(string: "https://m-api.xcar.com.cn")!
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .carInfo:
+            return "/newcar/Car/carinfo"
+        }
+    }
+    var method: Moya.Method {
+        switch self {
+        case .carInfo:
+            return .get
+        }
+    }
+    var task: Task {
+        switch self {
+        case let .carInfo(carId, cityId):
+            return .requestParameters(parameters: ["carId": carId, "cityId": cityId], encoding: URLEncoding.queryString)
+        }
+    }
+    var headers: [String: String]? {
+        switch self {
+        case let .carInfo(carId, cityId):
+            let _parameters = ["carId": carId, "cityId": cityId]
+            let token = Token(signKey: "x*&c%a&r^*2$0*1&^6*&k$e*%y*", parameter: _parameters).token
+            return ["token": token]
+        }
+    }
+    // 用于单元测试
+    var sampleData: Data {
+        switch self {
+        case .carInfo:
+            return "Half measures are as bad as nothing at all.".utf8Encoded
+        }
+    }
+}
